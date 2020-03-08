9a3191d849dc04c88fb6374a0f19c562cc6b4700
tiffhc <46658494+tiffhc@users.noreply.github.com>
2020-03-07 22:58:16 -0500

    Update player

--- Assets/Custom_Scripts/PlayerControl.cs
+++ Assets/Custom_Scripts/PlayerControl.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     //private attributes 
+
+    GameObject player; 
     private Animator _animator;
 
     private Vector3 _moveDirection = Vector3.zero; 
@@ -23,18 +25,16 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        player.transform.position += (move * Speed) * Time.deltaTime; 
+        
         //get Input for axis (up down left right)
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        Vector3 camForward_Dir = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
-        Vector3 move = v * camForward_Dir + h * Camera.main.transform.right; 
-        
-        if (move.magnitude < 1f)
-        {
-            move.Normalize(); 
-        }
-
+   
         _animator.SetBool("run", move.magnitude > 0); 
         
     }
