20a206cab0314b1821eeb6e1326010bed90858b6
tiffhc <46658494+tiffhc@users.noreply.github.com>
2020-03-07 22:33:46 -0500

    Made new custom animator folder

--- Assets/Custom_Scripts/PlayerControl.cs
+++ Assets/Custom_Scripts/PlayerControl.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+
+    public float Speed = 2.5f;
+
+    public float RotationSpeed = 240.0f;
+
+    public float JumpSpeed = 7.0f; 
     // Start is called before the first frame update
     void Start()
     {
